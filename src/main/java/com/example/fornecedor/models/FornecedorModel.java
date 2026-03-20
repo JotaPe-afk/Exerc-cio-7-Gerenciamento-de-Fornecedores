c076c90c21511bd675860ec4fa3c15ad6d0b234b
ioorgut <gustavoc2005@gmail.com>
2026-03-19 22:02:46 -0300

    Model (entity) criado

---
+++ src/main/java/com/example/fornecedor/models/FornecedorModel.java
@@ -0,0 +1,54 @@
+package com.example.fornecedor.models;
+
+import jakarta.persistence.*;
+
+@Entity(name = "Fornecedor")
+@Table(name = "tb_fornecedor")
+public class FornecedorModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "Nome_Fantasia")
+    private String nomeFantasia;
+    @Column(name = "CNPJ")
+    private String cnpj;
+    @Column(name = "Contato")
+    private String contato;
+
+    public FornecedorModel() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNomeFantasia() {
+        return nomeFantasia;
+    }
+
+    public void setNomeFantasia(String nomeFantasia) {
+        this.nomeFantasia = nomeFantasia;
+    }
+
+    public String getContato() {
+        return contato;
+    }
+
+    public void setContato(String contato) {
+        this.contato = contato;
+    }
+
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
+}
